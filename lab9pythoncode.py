f310a171c36afb9251f73768119417aeb9e5c396
bradfordrmc <77623911+bradfordrmc@users.noreply.github.com>
2021-03-03 20:07:31 -0400

    Update lab9pythoncode.py

--- lab9pythoncode.py
+++ lab9pythoncode.py
@@ -1,23 +1,23 @@
-from time import sleep
+from time import sleep #importing libraies needed for program
 import guizero
 import serial
 from guizero import TextBox, App, PushButton, Slider
 
-montr= serial.Serial("/dev/ttyACM0", 9600)
+montr= serial.Serial("/dev/ttyACM0", 9600) #setup serialmonitor
 montr.flush()
 sleep(3)
 
 Serv1pos=[]
 Serv2pos=[]
 
-def savepos(): 
+def savepos(): #function for storing slidervals
     Serv1pos.append(servsetting1.value)
     Serv2pos.append(servsetting2.value)
     textbox1.value = servsetting1.value
     textbox2.value = servsetting2.value
     
     
-def sliderread(sliderval1):
+def sliderread(sliderval1): #logic for moving servo1
     print(sliderval1)
     montr.flush()
     montr.write(str('M1').encode('utf-8'))
@@ -28,7 +28,7 @@ def sliderread(sliderval1):
     sleep(.2)
     
 
-def sliderread2(sliderval2):
+def sliderread2(sliderval2): #logic for moving servo2
     print(sliderval2)
     montr.flush()
     montr.write(str('M2').encode('utf-8'))
@@ -39,7 +39,7 @@ def sliderread2(sliderval2):
     sleep(.2)
 
 
-def repeatpos():
+def repeatpos(): #function for returning servos to stored val
     for i in range(0,len(Serv1pos)):
         montr.write(str('M1').encode('utf-8'))
         montr.write(str("\n").encode('utf-8'))
@@ -56,7 +56,7 @@ def repeatpos():
         montr.flush()
 
 
-app = App("Lab9")
+app = App("Lab9") #starting and settingup app 
 
 storebutton = PushButton(app, text="Store", command=savepos)
 repeatbutton = PushButton(app, text="Repeat", command=repeatpos)
